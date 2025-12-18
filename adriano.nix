3ef1c0910394b063d1d1a31b0b0b0e43e54af1b9
Friedtoast13 <adrianofullerton@gmail.com>
2025-12-18 14:50:25 -0500

    nvidia drivers installed

--- adriano.nix
+++ adriano.nix
@@ -6,12 +6,4 @@
      userName = "Friedtoast13";
      userEmail = "adrianofullerton@gmail.com"; 
   };
-    programs.vscode = {
-	  enable = true;
-	  extensions = with pkgs.vscode-extensions; [
-	    dracula-theme.theme-dracula
-	    vscodevim.vim
-	    yzhang.markdown-all-in-one
-	  ];
-	};
 }
