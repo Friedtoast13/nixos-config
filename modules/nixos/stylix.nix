a2ac088cd8dfc5b5d4dc573331b4fb1285a0b9f9
Friedtoast13 <adrianofullerton@gmail.com>
2025-12-24 02:31:49 -0500

    fixed

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -7,5 +7,5 @@
 
   # need to set default themes for nixos:
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
-  stylix.image = ../../../.assets/van-sickle.jpg;
+  stylix.image = /TtheC.jpg;
 }
